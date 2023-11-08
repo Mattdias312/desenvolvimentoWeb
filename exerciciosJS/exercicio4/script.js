b73ebb0d0c3cd76ff6b82fd15818c3dece976451
Mattdias312 <matheusf.dias12@gmail.com>
2023-11-08 09:12:37 -0300

    ajuste exercico 4

--- exerciciosJS/exercicio4/script.js
+++ exerciciosJS/exercicio4/script.js
@@ -1,20 +1,49 @@
 function calcular(){
-    let nascimento = 0.00;
+    let nascimento;
     nascimento = document.querySelector("input[name='nascimento'").value;
     let splitNasc = nascimento.split('-')
     hoje = new Date()
     let splithoje = hoje.toLocaleDateString().split('/')
+    if(splithoje[1] >= splitNasc[1]){
+        anos = (splithoje[2] - splitNasc[0])
+        meses = splithoje[1] - splitNasc[1]
+    }
+    else{
+        meses = (Number(splithoje[1]) + 12) - splitNasc[1]
+        anos = (splithoje[2] - splitNasc[0])-1
+    }
 
-    anos = splithoje[2] - splitNasc[0]
-    if(splithoje[1] >= splitNasc[1])
-    meses = splithoje[1] - splitNasc[1]
-    else
-    meses = (splithoje[1] + 12) - splitNasc[1]
+    if(splithoje[0] >= splitNasc[2]){
+        dias = splithoje[0] - splitNasc[2]
+    }
+    else if(splithoje[1] == 2){
+        dias = (Number(splithoje[0]) + 28) - splitNasc[2]
+    }
+    else if(splithoje[0] == 4 || splithoje[0] == 6 || splithoje[0] == 9 || splithoje[0] == 11){
+        dias = (Number(splithoje[0]) + 30) - splitNasc[2]
+    }
+    else{
+        dias = (Number(splithoje[0]) + 31) - splitNasc[2]
+    }
 
-    if(splithoje[0] >= splitNasc[2])
-    dias = splithoje[0] - splitNasc[2]
-    else if(splitNasc[1] == 2)
-    dias = splithoje[0] - splitNasc[2]
+    let horario = document.querySelector("input[name='hora'").value;
+    let hora = horario.split(":")
+    let horaHojeSplit = hoje.toLocaleTimeString().split(":")
+    if(horaHojeSplit[0] < hora[0]){
+        horas = (Number(horaHojeSplit[0]) + 24) - hora[0]
+        dias += -1
+    }
+    else{
+        horas = horaHojeSplit[0] - hora[0]
+    }
+    if(horaHojeSplit[1] < hora[1]){
+        minutos = (Number(horaHojeSplit[1]) + 60) - hora[1]
+    }
+    else{
+        minutos = horaHojeSplit[1] - hora[1]
+    }
+    
+    console.log(horas,minutos)
     console.log(anos)
     console.log(meses)
     console.log(dias)
